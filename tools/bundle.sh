73a6d7a236b2cadf59178496cf170ae1c2967da7
4yman <itcanbeanythingreally@gmail.com>
2024-07-06 23:00:57 +0100

    echo in bundle.sh

--- tools/bundle.sh
+++ tools/bundle.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Add note to main.js
-printf "// This file is generated AUTOMATICALLY\n\n" > js-min/main.js
+# Overwrite note to main.js
+printf "// This file is generated AUTOMATICALLY\n\n" > js-min/main.js \
+ && echo "main.js overwritten."
 
 for file in js-src/*.js js-src/**/*.js; do
-    cat $file >> js-min/main.js
+    cat $file >> js-min/main.js \
+     && echo "Added content from $file to main.js"
 done
